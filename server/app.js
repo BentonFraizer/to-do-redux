6b379bb100488448db37fca87b428ad2f21d2ecd
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2023-05-14 20:40:42 +0300

    Выполнена очистка (лишние файлы, роуты и т.п.)

--- server/app.js
+++ server/app.js
@@ -1,19 +1,7 @@
 const express = require('express');
-// const session = require('express-session');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const path = require('path');
 require('dotenv').config();
 
-// const sessionConfig = require('./config/session');
-// const mainRouter = require('./routes/views/main.routes');
-// const authRouter = require('./routes/views/auth.routes');
-// const authApiRouter = require('./routes/api/authApi.routes');
-// const questionsApiRouter = require('./routes/api/questionsApi.routes');
-// const themesApiRouter = require('./routes/api/themesApi.routes');
-// const historyRouter = require('./routes/api/history.routes');
 const tasksRouter = require('./routes/api/tasksApi.routes');
-// const adsApiRouter = require('./routes/api/ads.routes');
 const serverConfig = require('./config/serverConfig');
 
 const app = express();
@@ -21,12 +9,6 @@ const app = express();
 serverConfig(app);
 
 // роутинг
-// app.use(mainRouter);
-// app.use('/auth', authRouter);
-// app.use('/api/auth', authApiRouter);
-// app.use('/api/questions', questionsApiRouter);
-// app.use('/api/themes', themesApiRouter);
-// app.use('/api/history', historyRouter);
 app.use('/api/tasks', tasksRouter);
 
 const { PORT } = process.env;
